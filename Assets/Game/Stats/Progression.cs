76615b9f5076f28d8926090f27e0699da9dbaca7
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2023-06-11 18:59:38 +0200

    Nested class for proggresion inspector

---
+++ Assets/Game/Stats/Progression.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Progression", menuName = "Stats/Progression", order = 0)]
+public class Progression : ScriptableObject {
+  
+    }
